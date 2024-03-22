3dc1656bb2471b1545899911eca204fd29b55931
MatsRahmat <matsrahmathidayat@gmail.com>
2024-03-22 18:53:29 +0700

    pertemuan 4

---
+++ pertemuan4/index.php
@@ -0,0 +1,110 @@
+<?php
+
+// $counter = 0;
+
+// for(;$counter < 5;){
+//     echo "Hello for loop " . $counter . PHP_EOL;
+//     $counter++;
+//     echo "<br>";
+// }
+
+// for ($i = 0; $i < 10;) {
+//     echo "for ke dua " . $counter . PHP_EOL;
+//     echo "<br>";
+//     $i++;
+// }
+
+// for ($i = 0; $i < 10; $i++) {
+//     if ($i === 5) {
+//         echo "Ini loop pada hitungan ke {$i}";
+//         echo "<br>";
+//     } else {
+//         echo "for ke dua " . $i . PHP_EOL;
+//         echo "<br>";
+//         echo "<br>";
+//     }
+// }
+
+// $listMenu = ['nasgor', 'ubi', 'nasi bakar'];
+// for($i = 0; $i < count($listMenu); $i++){
+//     if($listMenu[$i] === 'ubi'){
+//         echo $i . PHP_EOL;
+//     }
+// }
+
+// for ($i = 0; $i < 4; $i++) :
+//     echo "Dengan endfor " . $i;
+//     echo "<br>";
+//     $var1 = "q";
+// endfor;
+
+
+// while ($couter < 6) {
+//     echo "While loop {$couter}";
+//     echo "<br>";
+//     echo "<hr>";
+//     $couter++;
+// }
+
+
+// while ($couter < 6) :
+//     echo "While loop {$couter}";
+//     echo "<br>";
+//     echo "<hr>";
+//     $couter++;
+// endwhile;
+
+// $couter = 1;
+
+// do {
+//     $couter++;
+//     echo "Ini do while {$couter}";
+//     echo "<br>";
+//     echo "<hr>";
+//     if($couter === 5) break;
+// } while (True);
+
+// for ($i = 0; $i < 10; $i++) {
+//     if ($i % 2 == 0) {
+//         continue;
+//     }
+//     echo "Hello continue {$i}";
+//     echo "<hr>";
+// }
+
+
+// $names = ['nofi', 'cahyono','mugiyarto'];
+
+// for($i = 0; $i < count($names); $i++){
+//     echo "Hello $names[$i]";
+//     echo "<br>";
+// }
+
+// foreach ($names as $name) {
+//     echo "Hello {$name}";
+//     echo "<br>";
+// }
+
+
+// $names = [
+//     'name depan' => 'nofi',
+//     'nama belakang' => 'cahyono'
+// ];
+
+// foreach($names  as $key => $value) {
+//     echo "{$key} : {$value}";
+//     echo "<hr>";
+// }
+
+// $counter = 1;
+// while (true) {
+//     echo "While loop {$counter}";
+//     echo "<hr>";
+//     $counter++;
+//     if ($counter > 5) {
+//         goto end;
+//     }
+// }
+
+// end:
+// echo "End loop";
